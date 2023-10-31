6ec6cbd03f47c2b13fa8976d71b6a7c4029279d2
Racheal Muchiri <rachealloveo6@gmail.com>
2023-10-31 22:39:50 +0300

    Update index.js

--- frontend/src/constants/index.js
+++ frontend/src/constants/index.js
@@ -5,7 +5,7 @@ import {
   payment,
   profile,                           
   withdraw,            
-} from "../assets";
+} from "../assets";                           
 
 export const navlinks = [
   {
